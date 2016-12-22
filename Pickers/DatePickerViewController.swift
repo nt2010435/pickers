4605e4d4fd3d469e6e6de0effba66f221c786a70
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2016-12-22 12:38:19 -0800

    Added UI and codes to date picker

--- Pickers/DatePickerViewController.swift
+++ Pickers/DatePickerViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class DatePickerViewController: UIViewController {
+    @IBOutlet weak var datePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,4 +33,14 @@ class DatePickerViewController: UIViewController {
     }
     */
 
+    @IBAction func onButtonPressed(_ sender: Any) {
+        let date = datePicker.date
+        let message = "The date and time you selected is \(date)"
+        let alert = UIAlertController(title: "Date and Time Selected", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "That's so true!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
 }
