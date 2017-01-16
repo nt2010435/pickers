84ad395e332ca94175b5da8d66a2331f482a2821
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2017-01-16 08:57:23 -0800

    Added picker, label, button and codes for the custom picker

--- Pickers/CustomPickerViewController.swift
+++ Pickers/CustomPickerViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AudioToolbox
 
-class CustomPickerViewController: UIViewController {
-
+class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    private var images:[UIImage]!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    private var winSoundID: SystemSoundID = 0
+    private var crunchSoundID: SystemSoundID = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+        images = [
+            UIImage(named: "seven")!,
+            UIImage(named: "bar")!,
+            UIImage(named: "crown")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "lemon")!,
+            UIImage(named: "apple")!
+        ]
+        winLabel.text = "Good Luck" // Note the space between the quotes
+        arc4random_stir()    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,5 +47,56 @@ class CustomPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func spin(_ sender: Any) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        
+        for i in 0..<5 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow += 1
+            } else {
+                numInRow = 1
+            }
+            lastVal = newValue
+            
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            if numInRow >= 3 {
+                win = true
+            }
+        }
+        winLabel.text = win ? "WINNER!" : " " //Note the space between the quotes
+    }
+    
+    
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    // MARK:-
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView,
+                    viewForRow row: Int,
+                    forComponent component: Int,
+                    reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+
+    }
 
 }
